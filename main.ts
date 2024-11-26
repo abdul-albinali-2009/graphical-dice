ae061267738d83354ec33ba1c797ec6226f1810e
abdul-albinali-2009 <abdulrahman.albinali@stu.ocsb.ca>
2024-11-26 10:18:43 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,56 @@
+let number = 0
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
+    
+    basic.clearScreen()
+    number = randint(1, 6)
+    if (number == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (number == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . . . #
+            . . . . .
+            . . . . .
+            `)
+    } else if (number == 3) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (number == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (number == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+    
+})
